f8afb43bedc36cbe68fe6ed8044be8565625b59a
Philip Clifton <me@philipmclifton.com>
2017-11-08 15:49:28 +0000

    Initial Commit

---
+++ WondeExtendSocialite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SocialiteProviders\Wonde;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class WondeExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('wonde', __NAMESPACE__ . '\Provider');
+    }
+}
