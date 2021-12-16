31fa3ec47c1edb78c937fde6d9196f6358ff9345
Sam D Harris <samdharris@outlook.com>
2021-12-16 12:55:35 +0000

    Add an enum for Person __typename property

---
+++ PersonTypeNameEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SocialiteProviders\Wonde;
+
+abstract class PersonTypeNameEnum
+{
+    const EMPLOYEE = 'Employee';
+    const STUDENT = 'Student';
+    const CONTACT = 'Contact';
+}
